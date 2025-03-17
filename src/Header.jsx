4b4d1bcbb3d5638b467f2e76e4dc70bfb938ad1b
abir718 <abir.hasan2003s@gmail.com>
2025-03-17 18:56:04 +0600

    changes to offer and header added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,49 +1,61 @@
-
+import { FiUser } from "react-icons/fi";
+import { LuUsers } from "react-icons/lu";
+import { RiCustomerServiceLine } from "react-icons/ri";
+import { GrMoney } from "react-icons/gr";
 
 const Header = () => {
     return (
         <header className="mx-auto w-[80%]">
-            <div className=" mt-14 flex items-center justify-between">
-        <h1 className="text-4xl font-medium">EasyRoute</h1>
-        <div className="flex items-center gap-8">
-            <ul className="text-gray-600 text-lg font-medium">Home</ul>
-            <ul className="text-gray-600 text-lg font-medium">About</ul>
-            <ul className="text-gray-600 text-lg font-medium">Destination</ul>
-            <ul className="text-gray-600 text-lg font-medium">Search</ul>
-        </div>
-            <div className="flex items-center gap-2 p-2 w-fit border-2 border-green-400 rounded-lg bg-green-100">
-                <p className="text-[#22c55e] text-lg font-medium">Bus</p>
-                <img src="/images/bus-icon.png" alt="" />
+            <div className="mt-4 flex items-center justify-between">
+                <h1 className="text-4xl font-medium">EasyRoute</h1>
+                <div className="hidden md:flex items-center gap-8">
+                    <ul className="text-gray-600 text-lg font-medium">Home</ul>
+                    <ul className="text-gray-600 text-lg font-medium">Bus Service</ul>
+                    <ul className="text-gray-600 text-lg font-medium">Coupons</ul>
+                </div>
+                <div className="flex items-center gap-2 p-2 w-fit border-2 border-green-400 rounded-lg bg-green-100">
+                    <p className="text-[#22c55e] text-lg font-medium">Bus</p>
+                    <img src="/images/bus-icon.png" alt="" />
+                </div>
             </div>
+            <div className="bg-[url('/images/banner.png')] bg-cover bg-center h-[600px] w-full flex flex-col items-center justify-evenly mt-10 bg-no-repeat rounded-3xl p-6 md:p-32">
+                <h1 className="text-4xl md:text-7xl font-bold text-white">End-To-End Travel With</h1>
+                <h1 className="text-4xl md:text-7xl font-bold text-[#22c55e]">EasyRoute</h1>
+                <p className="text-lg md:text-2xl text-gray-200 text-center">Yes, you can run unit tests and view the results directly within the app. The integrated testing features allow for a streamlined runtes.</p>
+                <button className="text-lg md:text-xl font-medium bg-[#22c55e] p-3 md:p-4 rounded-lg hover:bg-transparent border-2 border-[#22c55e]">Buy Tickets</button>
             </div>
-            <div className="bg-[url('/images/banner.png')] bg-cover bg-center h-[600px] w-full flex flex-col items-center justify-evenly mt-10 bg-no-repeat rounded-3xl p-32">
-            <h1 className="text-7xl font-bold text-white">End-To-End Travel With</h1>
-            <h1 className="text-7xl font-bold text-green-500">EasyRoute</h1>
-            <p className="text-2xl text-gray-200 text-center">Yes, you can run unit tests and view the results directly within the app. The integrated testing <br /> features allow for a streamlined .</p>
-            <button className="text-xl font-medium bg-green-500 p-4 rounded-lg hover:bg-transparent border-2 border-green-500">Buy Tickets</button>
-            </div>
-            <div className="flex items-center gap-10 absolute top-[720px] left-0 right-0 justify-center">
-                <div className="flex items-center gap-2 border-b-4 w-fit p-6 bg-white rounded-lg border-green-500">
-                    <img src="images/people.png" alt="" />
+            <h1 className="lg:text-4xl text-2xl text-center font-bold py-6 mt-8">Why Choose Us ?</h1>
+            <div className="flex flex-wrap items-center gap-4 md:gap-20 justify-center">
+                    
+                <div className="flex items-center gap-2 border-b-4 w-[280px] p-4 bg-white rounded-lg border-[#22c55e]">
+                    <LuUsers className="size-16 text-[#22c55e]" />
                     <div>
-                        <h1 className="text-4xl font-bold mb-2">500K+</h1>
-                        <p className="text-xl font-medium text-gray-500">Registered Users</p>
+                        <h1 className="text-2xl md:text-4xl font-bold mb-2">500K+</h1>
+                        <p className="text-lg md:text-xl font-medium text-gray-500">Registered Users</p>
                     </div>
                 </div>
-                <div className="flex items-center gap-2 border-b-4 w-fit p-6 bg-white rounded-lg border-green-500">
-                    <img src="images/people.png" alt="" />
+                <div className="flex items-center gap-2 border-b-4 w-[280px] p-4 bg-white rounded-lg border-[#22c55e]">
+                    <FiUser className="size-16 text-[#22c55e]"/>
                     <div>
-                        <h1 className="text-4xl font-bold mb-2">500K+</h1>
-                        <p className="text-xl font-medium text-gray-500">Registered Users</p>
+                        <h1 className="text-2xl md:text-4xl font-bold mb-2">25+</h1>
+                        <p className="text-lg md:text-xl font-medium text-gray-500">Ticket Outlet</p>
                     </div>
                 </div>
-                <div className="flex items-center gap-2 border-b-4 w-fit p-6 bg-white rounded-lg border-green-500">
-                    <img src="images/people.png" alt="" />
+                <div className="flex items-center gap-2 border-b-4 w-[280px] p-4 bg-white rounded-lg border-[#22c55e]">
+                    <RiCustomerServiceLine className="size-16 text-[#22c55e]"/>
                     <div>
-                        <h1 className="text-4xl font-bold mb-2">500K+</h1>
-                        <p className="text-xl font-medium text-gray-500">Registered Users</p>
+                        <h1 className="text-2xl md:text-4xl font-bold mb-2">24/7</h1>
+                        <p className="text-lg md:text-xl font-medium text-gray-500">Customer Care</p>
                     </div>
                 </div>
+                <div className="flex items-center gap-2 border-b-4 w-[280px] p-4 bg-white rounded-lg border-[#22c55e]">
+                    <GrMoney className="size-16 text-[#22c55e]"/>
+                    <div>
+                        <h1 className="text-2xl md:text-4xl font-bold mb-2">$$</h1>
+                        <p className="text-lg md:text-xl font-medium text-gray-500">Budget Friendly</p>
+                    </div>
+                </div>
+
             </div>
         </header>
     );
--- src/Offer.jsx
+++ src/Offer.jsx
@@ -1,36 +1,73 @@
+import { useState } from "react";
+
+
 
 const Offer = () => {
+
+    let [more, setMore] = useState(false)
+
+    let showMore = () => {
+        setMore(true)
+    }
+    console.log(more)
     return (
-      <section className="mt-52 mb-20 mx-auto w-[80%] flex flex-col items-center gap-10">
-        <h1 className="text-6xl font-bold">Best offers for you</h1>
-        <div className="flex items-center gap-52">
-        <div className="flex gap-6 items-center bg-yellow-400 w-[550px] px-16 rounded-2xl">
-            <div>
-                <h1 className="text-4xl font-bold py-1">15% OFF</h1>
-                <p className="text-xl font-medium">on your next purchase</p>
-                <p className="text-[rgba(0,0,0,0.5)]  text-lg font-medium py-1">Use by January 2024</p>
+        <section className="mt-20 mb-20 mx-auto w-[80%] flex flex-col items-center gap-10">
+            <h1 className="text-4xl font-bold">Best offers for you</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-44">
+                <div className="flex gap-6 items-center bg-yellow-400 w-full px-12 rounded-2xl">
+                    <div>
+                        <h1 className="text-xl lg:text-3xl font-bold py-1">15% OFF</h1>
+
+                        <p className="text-[rgba(0,0,0,0.5)]  text-lg font-medium py-1">Use by December 2025</p>
+                    </div>
+                    <img className="w-10" src="images/cupon-devider.png" alt="" />
+                    <div>
+                        <h1 className="text-xl lg:text-3xl font-bold py-1">NEW15</h1>
+                        <p className="text-[rgba(0,0,0,0.5)] text-lg font-medium">Coupon Code</p>
+                    </div>
+                </div>
+                <div className="flex gap-4 items-center bg-red-400 w-full px-12 rounded-2xl">
+                    <div>
+                        <h1 className="text-xl lg:text-3xl font-bold py-1">20% OFF</h1>
+
+                        <p className="text-[rgba(0,0,0,0.5)]  text-lg font-medium py-1">Use by December 2025</p>
+                    </div>
+                    <img className="w-10" src="images/cupon-devider.png" alt="" />
+                    <div>
+                        <h1 className="text-xl lg:text-3xl font-bold py-1">COUPLE20</h1>
+                        <p className="text-[rgba(0,0,0,0.5)] text-lg font-medium">Coupon Code</p>
+                    </div>
+                </div>
+
+
             </div>
-            <img className="w-10" src="images/cupon-devider.png" alt="" />
-            <div>
-                <h1 className="text-3xl font-bold py-1">NEW15</h1>
-                <p className="text-[rgba(0,0,0,0.5)] text-lg font-medium">Coupon Code</p>
-            </div>
-        </div>
-        <div className="flex gap-4 items-center bg-red-400 w-[550px] px-16 rounded-2xl">
-            <div>
-                <h1 className="text-4xl font-bold py-1">20% OFF</h1>
-                <p className="text-xl font-medium">on your next purchase</p>
-                <p className="text-[rgba(0,0,0,0.5)]  text-lg font-medium py-1">Use by January 2024</p>
-            </div>
-            <img className="w-10" src="images/cupon-devider.png" alt="" />
-            <div>
-                <h1 className="text-3xl font-bold py-1">COUPLE20</h1>
-                <p className="text-[rgba(0,0,0,0.5)] text-lg font-medium">Coupon Code</p>
-            </div>
-        </div>
-        </div>
-        <button className="text-green-500 font-medium text-2xl border-2 border-green-500 p-4 rounded-lg hover:bg-green-100">See All Offers</button>
-      </section>
+            {more ? (
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-44">
+                    <div className="flex gap-4 items-center bg-green-400 w-full px-12 rounded-2xl">
+                        <div>
+                            <h1 className="text-xl lg:text-3xl font-bold py-1">30% OFF</h1>
+                            <p className="text-[rgba(0,0,0,0.5)]  text-lg font-medium py-1">Use by December 2025</p>
+                        </div>
+                        <img className="w-10" src="images/cupon-devider.png" alt="" />
+                        <div>
+                            <h1 className="text-xl lg:text-3xl font-bold py-1">EASY30</h1>
+                            <p className="text-[rgba(0,0,0,0.5)] text-lg font-medium">Coupon Code</p>
+                        </div>
+                    </div>
+                    <div className="flex gap-4 items-center bg-blue-400 w-full px-12 rounded-2xl">
+                        <div>
+                            <h1 className="text-xl lg:text-3xl font-bold py-1">20% OFF</h1>
+                            <p className="text-[rgba(0,0,0,0.5)]  text-lg font-medium py-1">Use by December 2025</p>
+                        </div>
+                        <img className="w-10" src="images/cupon-devider.png" alt="" />
+                        <div>
+                            <h1 className="text-xl lg:text-3xl font-bold py-1 ">COUPLE20</h1>
+                            <p className="text-[rgba(0,0,0,0.5)] text-lg font-medium">Coupon Code</p>
+                        </div>
+                    </div>
+                </div>) : ""}
+            <button onClick={showMore} className={` ${more ? "hidden" : ""} text-green-500 font-medium text-2xl border-2 border-green-500 p-4 rounded-lg hover:bg-green-100`}>See All Offers</button>
+        </section>
     );
 };
 
